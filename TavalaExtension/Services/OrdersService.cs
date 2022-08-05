2dd05e3af62d48b15097a3ce317e795aaa74d092
Praveen Madhav <praveen@directscale.com>
2022-08-05 16:16:07 +0530

    Updates to Order Service

--- TavalaExtension/Services/OrdersService.cs
+++ TavalaExtension/Services/OrdersService.cs
@@ -2,6 +2,7 @@
 using TavalaExtension.Repositories;
 using DirectScale.Disco.Extension.Services;
 using DirectScale.Disco.Extension.Hooks.Orders;
+using DirectScale.Disco.Extension;
 
 namespace TavalaExtension.Services
 {
@@ -14,16 +15,19 @@ namespace TavalaExtension.Services
     {
         private readonly IOrdersRepository _ordersRepository;
         private readonly IOrderService _orderService;
-        public OrdersService(IOrdersRepository ordersRepository, IOrderService orderService)
+        private readonly IItemService _itemService;
+
+        public OrdersService(IOrdersRepository ordersRepository, IOrderService orderService, IItemService itemService)
         {
             _ordersRepository = ordersRepository ?? throw new ArgumentNullException(nameof(ordersRepository));
             _orderService = orderService ?? throw new ArgumentNullException(nameof(orderService));
+            _itemService = itemService ?? throw new ArgumentNullException(nameof(itemService));
         }
         public SubmitOrderHookRequest AddSkuToOrder(SubmitOrderHookRequest request)
         {
 
-            if(_ordersRepository.CheckIfFirstOrder(request.Order.AssociateId))
-              {
+            if(_ordersRepository.CheckIfFirstOrder(request.Order.AssociateId)== false)
+            {
                     bool sku275Exists = false;
                     foreach (var item in request.Order.LineItems)
                     {
@@ -34,13 +38,24 @@ namespace TavalaExtension.Services
                         }
                     }
 
-                if (sku275Exists == false)
-                {
-                    var lineItem = request.Order.LineItems[0];
-                    //lineItem.
+                    if (sku275Exists == false)
+                    {
 
+                        
+                        LineItem[] newSkus= request.Order.LineItems;
 
-                }
+                        // We will resize the original array and increase the length by 1
+                        Array.Resize(ref newSkus, newSkus.Length + 1);
+
+                        LineItem lineItemEntry = request.Order.LineItems[0];
+                        lineItemEntry.ItemId = 275;
+                        lineItemEntry.Quantity = 1;
+
+                        // Adding a new element by adding a new item on the last element
+                        newSkus[newSkus.Length - 1] = lineItemEntry;
+
+                        request.Order.LineItems = newSkus;
+                    }
             }
             return request;
         }
